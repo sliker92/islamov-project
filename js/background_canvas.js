e80baa26d344406f300895c5c52a7358e0c62069
sliker92 <sliker1992@gmail.com>
2019-06-20 01:39:32 +0300

    ready background for menu

--- js/background_canvas.js
+++ js/background_canvas.js
@@ -1,27 +1,42 @@
 var canvas = document.getElementById('background_canvas');
 var context = canvas.getContext('2d');
+var squareArray = []; // массив в который добавляются квадраты, для последующей их отрисовки
+var colors = ['rgba(251, 214, 205, 0.3)', 'rgba(255, 50, 0, 0.3)',
+  'rgba(55, 55, 55, 0.3)', 'rgba(167, 167, 167, 0.3)' ];  // массив цветов для квадратов
+var sizes = [10, 20, 5]; // массив размеров для квадратов
+canvas.style.display = 'block';
+canvas.style.backgroundColor = '#291414';
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-function Square(x, y, w, h, transformX, transformY) {
+// Функция конструктор для квадратов
+function Square(x, y, w, h, transformX, transformY, color, diagonal) {
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
   this.transformX = transformX;
   this.transformY = transformY;
+  this.color = color;
+  this.diagonal = diagonal;
 }
 
+//  Методы объекта
+
+//  отрисовывает квадраты на основе данных переданных при создании объекта с помощью ф-ии кон-ра
 Square.prototype.draw = function () {
-  context.strokeRect(this.x, this.y, this.w, this.h);
   context.fillRect(this.x, this.y, this.w, this.h);
+  context.fillStyle = this.color;
 };
 
+// обновляет отрисовку на основе координат
 Square.prototype.update = function () {
-  if (this.x + this.diagonal < innerWidth || this.x - this.diagonal < 0) {
+  // условия столкновения с осью X по краям экрана
+  if (this.x + this.diagonal > innerWidth || this.x < 0) {
     this.transformX = -this.transformX;
   }
-  if (this.y + this.diagonal > innerHeight || this.y - this.diagonal < 0) {
+  // условия столкновения с осью Y по краям экрана
+  if (this.y + this.diagonal > innerHeight || this.y < 0) {
     this.transformY = -this.transformY;
   }
 
@@ -31,28 +46,37 @@ Square.prototype.update = function () {
   this.draw();
 };
 
-var squareArray = [];
-
-for (var i = 0; i < 1000; i++) {
-  var diagonal = 30;
-  var w = 20;
-  var h = 20;
-  var x = Math.random() * (innerWidth - diagonal * 2) + diagonal;
-  var y = Math.random() * (innerHeight - diagonal * 2) + diagonal;
-  var transformX =  (Math.random() - 0.5);
-  var transformY =  (Math.random() - 0.5);
-  squareArray.push((new Square(x, y, w, h, transformX, transformY)));
+// функция принимает необходимое количество квадратов и создаёт их при помощи ф-ии кон-ра
+function init(squares) {
+  for (var i = 0; i < squares; i++) {
+    var diagonal = 30;
+    var w = randomSize(sizes);
+    var h = w;
+    var x = Math.random() * (innerWidth - diagonal * 2) + diagonal;
+    var y = Math.random() * (innerHeight - diagonal * 2) + diagonal;
+    var transformX =  (Math.random() - 0.5);
+    var transformY =  (Math.random() - 0.5);
+    var color = randomColor(colors);
+    squareArray.push((new Square(x, y, w, h, transformX, transformY, color, w)));
+  }
 }
 
 function animate() {
   requestAnimationFrame(animate);
-  context.clearRect(0, 0 , innerWidth, innerHeight);
-
+  context.clearRect(0, 0, innerWidth, innerHeight); // очищает заданную область
   for (var i = 0; i < squareArray.length; i++) {
     squareArray[i].update();
   }
 }
 
+// Функции для подбора случайных значений данных
+function randomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function randomSize() {
+  return sizes[Math.floor(Math.random() * sizes.length)];
+}
+
 animate();
-var square = new Square();
-square.draw();
+init(2000);
